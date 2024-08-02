a5b973506228a1eef6eb496d411c02d3234d36b6
rafabcanedo <rafaitecanedo@gmail.com>
2024-08-02 17:00:37 -0300

    teste carrosel with loading image

--- src/components/carrosel/carrosel.tsx
+++ src/components/carrosel/carrosel.tsx
@@ -1,9 +1,17 @@
 "use client"
 import React, { useCallback } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules'
 import useEmblaCarousel from 'embla-carousel-react'
-import Autoplay from 'embla-carousel-autoplay'
 import Image from 'next/image'
 import { ArrowLeft, ArrowRight } from '@phosphor-icons/react'
+
+import 'swiper/css'
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import 'swiper/css/scrollbar'
+import 'swiper/css/effect-fade'
 
 export function Carrosel() {
 
@@ -29,39 +37,7 @@ export function Carrosel() {
   ]
 
   return (
-    <div className="embla flex flex-col items-center justify-center max-w-96 sm:max-w-96 md:max-w-7xl md:pt-16 md:pb-4 md:m-auto">
-    <div className="embla__viewport mx-auto mt-12 max-w-lg border h-56" ref={emblaRef}>
-      <div className="embla__container h-full">
-        <div className="embla__slide flex items-center justify-center">
-        {data.map((item) => (
-         <>
-        <div key={item.id}>
-       <Image
-        src={item.image}
-        alt="Imagens Modelo"
-        className="slide-item"
-       />
-       </div>
-       </>
-  ))}
-        </div>
-      </div>
-    </div>
-
-    <div className='flex flex-row gap-6'>
-     <button className="embla__prev" onClick={scrollPrev}>
-     <ArrowLeft className='text-primary' size={25} />
-     </button>
-     <button className="embla__next" onClick={scrollNext}>
-      <ArrowRight className='text-primary' size={25} />
-     </button>
-    </div>
-    </div>
-  )
-}
-
-/*
-<div className="flex items-center justify-center max-w-96 sm:max-w-96 md:max-w-7xl md:pt-16 md:pb-4 md:m-auto">
+    <div className="flex items-center justify-center max-w-96 sm:max-w-96 md:max-w-7xl md:pt-16 md:pb-4 md:m-auto">
     <Swiper
       effect={'coverflow'}
       grabCursor={true}
@@ -86,6 +62,7 @@ export function Carrosel() {
       src={item.image}
       alt="Imagens Modelo"
       className="slide-item"
+      loading="lazy"
      />
     </SwiperSlide>
     </>
@@ -96,4 +73,38 @@ export function Carrosel() {
       </div>
     </Swiper>
   </div>
+  )
+}
+
+/*
+<div className="embla flex flex-col items-center justify-center max-w-96 sm:max-w-96 md:max-w-7xl md:pt-16 md:pb-4 md:m-auto">
+    <div className="embla__viewport" ref={emblaRef}>
+      <div className="embla__container h-full">
+        <div className="embla__slide flex items-center justify-center">
+        {data.map((item) => (
+         <>
+        <div key={item.id}>
+       <Image
+        src={item.image}
+        alt="Imagens Modelo"
+        className="embla__img"
+       />
+       </div>
+       </>
+  ))}
+        </div>
+      </div>
+    </div>
+
+    <div className='flex flex-row gap-6'>
+     <button className="embla__prev" onClick={scrollPrev}>
+     <ArrowLeft className='text-primary' size={25} />
+     </button>
+     <button className="embla__next" onClick={scrollNext}>
+      <ArrowRight className='text-primary' size={25} />
+     </button>
+    </div>
+    </div>
 */
+
+// className="embla__viewport mx-auto mt-12 max-w-lg border h-56" ref={emblaRef}
